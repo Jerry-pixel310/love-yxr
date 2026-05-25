bdb1baf6ddbe521f250342f2d4d13786acbf6c47
Jerry-pixel310 <Jerry-pixel310@users.noreply.github.com>
2026-05-25 21:35:52 +0800

    chore: 配置 GitHub Pages 部署

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/love-letter-ranran/',
   plugins: [react()],
   test: {
     globals: true,
