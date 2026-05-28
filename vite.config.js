55d869baa73149a7d6e9a66adf4ba1d6f46f7eae
Jerry-pixel310 <Jerry-pixel310@users.noreply.github.com>
2026-05-28 11:09:18 +0800

    chore: 更新仓库名为 love-yxr

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/love-letter-ranran/',
+  base: '/love-yxr/',
   plugins: [react()],
   test: {
     globals: true,
