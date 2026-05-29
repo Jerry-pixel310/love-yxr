88542d4586bc9a1c947328aa89b1b7315b327fc6
Jerry-pixel310 <Jerry-pixel310@users.noreply.github.com>
2026-05-29 11:03:07 +0800

    Updates

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE = 'love-letter-v1'
+const PRECACHE = [
+  '/love-yxr/',
+  '/love-yxr/index.html',
+]
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then((c) => c.addAll(PRECACHE))
+      .then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return
+  const url = new URL(e.request.url)
+  if (url.pathname.endsWith('.html') || url.pathname === '/love-yxr/') {
+    e.respondWith(
+      fetch(e.request).catch(() => caches.match(e.request))
+    )
+  } else {
+    e.respondWith(
+      caches.match(e.request).then((cached) =>
+        cached || fetch(e.request).then((res) => {
+          const clone = res.clone()
+          caches.open(CACHE).then((c) => c.put(e.request, clone))
+          return res
+        })
+      )
+    )
+  }
+})
